5836d862b262a46e69a0fb4cbd54400a11710742
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-16 17:49:27 -0800

    added sign up and sign in events

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 /* Imports */
 
+import { signUpUser } from './fetch-utils.js';
+
 /* Get DOM Elements */
+const signInForm = document.getElementById('sign-in');
+const signUpForm = document.getElementById('sign-up');
 
 /* State */
 
 /* Events */
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const user = await signUpUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('/other-page');
+    }
+});
 
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signUpUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('/other-page');
+    }
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
--- fetch-utils.js
+++ fetch-utils.js
@@ -17,3 +17,5 @@ export async function signInUser(email, password) {
     const response = await client.auth.signIn({ email, password });
     return response.user;
 }
+
+// export async function checkAuth() {}
