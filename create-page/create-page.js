73450b961f19535ac6b08eeec575146257038569
Harold Jean <haroldjean38@yahoo.com>
2022-11-17 17:45:47 -0500

    added home page button to create page

--- create-page/create-page.js
+++ create-page/create-page.js
@@ -3,3 +3,9 @@ import { checkAuth } from '../fetch-utils.js';
 window.addEventListener('load', async () => {
     checkAuth();
 });
+
+const homePageButton = document.getElementById('home-page');
+
+homePageButton.addEventListener('click', () => {
+    location.replace('/');
+});
