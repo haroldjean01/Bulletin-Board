2c4801b077fa3f86b203a34dcbdc831283f1665c
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-16 17:33:41 -0800

    added ASYNC user login functions

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,17 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzc3NjZGJibHFsYWd2Z3Z2dGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwMTYsImV4cCI6MTk4MzY4NDAxNn0.ZLiT0DIvTuegM8zzRIvSgVzLLFRk-1PwYGXz7BhqKk4';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signUpUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+    return response.user;
+}
