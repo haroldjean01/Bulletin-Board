204806903a50da23aba68159aa441fa2c666be03
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-17 14:21:20 -0800

    moved login page to its own page and added button

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 /* Imports */
 
-import { signUpUser, signInUser } from './fetch-utils.js';
+// import { signUpUser, signInUser } from './fetch-utils.js';
 
 /* Get DOM Elements */
-const signInForm = document.getElementById('sign-in');
-const signUpForm = document.getElementById('sign-up');
+// const signInForm = document.getElementById('sign-in');
+// const signUpForm = document.getElementById('sign-up');
+const loginButton = document.getElementById('login-button');
+const createButton = document.getElementById('create-button');
 
 /* State */
 
 /* Events */
-signUpForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const data = new FormData(signUpForm);
-    const user = await signUpUser(data.get('email'), data.get('password'));
-    if (user) {
-        location.replace('/other-page');
-    }
+loginButton.addEventListener('click', () => {
+    location.replace('/login-page');
 });
+// signUpForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
+//     const data = new FormData(signUpForm);
+//     const user = await signUpUser(data.get('email'), data.get('password'));
+//     if (user) {
+//         location.replace('/other-page');
+//     }
+// });
 
-signInForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const data = new FormData(signInForm);
-    const user = await signInUser(data.get('email'), data.get('password'));
-    if (user) {
-        location.replace('/other-page');
-    }
-});
+// signInForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
+//     const data = new FormData(signInForm);
+//     const user = await signInUser(data.get('email'), data.get('password'));
+//     if (user) {
+//         location.replace('/other-page');
+//     }
+// });
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
